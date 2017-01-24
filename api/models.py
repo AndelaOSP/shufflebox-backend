1b46e84b70017c570cdcd99b3511c5e9618bac1d
Jee Githinji Gikera <githinjigikera@gmail.com>
2017-01-24 23:11:35 +0300

    [Feature #138032761] define profile model that extends default django user model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.dispatch import receiver
 
-# Create your models here.
+
+class Profile(models.Model):
+    """Class definition for the User Profile model.
+
+    Extends default django User model that stores username, email, pword etc
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = models.CharField(blank=False)
+    birth_date = models.DateField(null=True, blank=True)
+    bio = models.TextField(default="", max_length=500, blank=True)
+
+    def __unicode__(self):
+        return u'User Profile for: {}'.format(self.user.username)
+
+
+# Decorator to pass in a post_save signal
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    """Signal to create a user profile when a User instance is created."""
+
+    if created:
+        Profile.objects.create(user=instance)
+
+
+# Decorator to pass in a post_save signal
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    """Signal to update a user profile when a User instance is updated."""
+    instance.profile.save()
