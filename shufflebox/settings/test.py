43f8fcf107227819f7e3b85ebb5f31ead401d8f0
Jack Mwangi <jack.wachira@andela.com>
2017-01-25 14:25:05 +0300

    (chore) Modify settings

---
+++ shufflebox/settings/test.py
@@ -0,0 +1,12 @@
+"""
+Test specific settings.
+"""
+
+from .base import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(os.path.dirname(__file__), 'test.db')
+    }
+}
