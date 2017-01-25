31b518153ec1584fa82c45a0f03ed9cccc767958
Jack Mwangi <jack.wachira@andela.com>
2017-01-25 14:25:05 +0300

    (chore) Modify settings

---
+++ shufflebox/settings/development.py
@@ -0,0 +1,19 @@
+"""
+Development specific settings.
+"""
+
+from .base import *
+
+# Database
+# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'shufflebox',
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
+    }
+}
