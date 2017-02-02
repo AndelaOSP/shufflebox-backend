a714b22b3834f6588d1b9dcd394aca6d34237947
Jee Githinji Gikera <githinjigikera@gmail.com>
2017-02-02 14:48:01 +0300

    (chore) edit admin sites

--- api/admin.py
+++ api/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from .models import Profile, BrownBag, SecretSanta, UserHangout, Hangout
+from .models import Profile, BrownBag, SecretSanta, Hangout
 
 # Register your models here.
 admin.site.register(Profile)
 admin.site.register(BrownBag)
 admin.site.register(Hangout)
-admin.site.register(UserHangout)
 admin.site.register(SecretSanta)
