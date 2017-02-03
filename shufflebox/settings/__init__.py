32d1f6bdd9548c2a00e286f6d9aa72823d6e3d97
Jee Githinji Gikera <githinjigikera@gmail.com>
2017-02-03 14:36:44 +0300

    [Feature #138140313] Add coverage

--- shufflebox/settings/__init__.py
+++ shufflebox/settings/__init__.py
@@ -4,15 +4,19 @@ Settings package initialization.
 """
 
 import os
+from dotenv import load_dotenv
 
 # Ensure development settings are not used in testing and production:
 if not os.getenv('CI') and not os.getenv('HEROKU'):
     # load and set environment variables from '.env.yml' or '.env.py' files
     # with django_envie
-    from dotenv import load_dotenv
+
     load_dotenv('.env')
 
     from shufflebox.settings.development import *
 
 if os.getenv('HEROKU'):
-    from production import *
+    from shufflebox.settings.production import *
+
+if os.getenv('TRAVIS_BUILD'):
+    from shufflebox.settings.test import *
