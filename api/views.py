1bdefee511ae4efc1abc0cfaff1bd8af5dc76630
Jee Githinji Gikera <githinjigikera@gmail.com>
2017-02-07 22:00:47 +0300

    [Feature-138140325] add a user view for listing all users

--- api/views.py
+++ api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from .models import BrownBag, Hangout, SecretSanta, Profile
+from .serializers import UserSerializer, ProfileSerializer, \
+    BrownbagSerializer, HangoutSerializer, SecretSantaSerializer
+from rest_framework import generics,
+from django.contrib.auth.models import User
 
-# Create your views here.
+
+class UserView(generics.ListAPIView):
+    """This class creates a concrete view for listing current users."""
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
