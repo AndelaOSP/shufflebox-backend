d45e71220f8de683ca3d34f390b0a50cbe8d32df
Jee Githinji Gikera <githinjigikera@gmail.com>
2017-02-10 17:33:17 +0300

    [Feature #138140325] Define views and their corresponding tests initial skeleton

--- api/views.py
+++ api/views.py
@@ -5,7 +5,25 @@ from rest_framework import generics,
 from django.contrib.auth.models import User
 
 
-class UserView(generics.ListAPIView):
-    """This class creates a concrete view for listing current users."""
+class UserView(generics.ListCreateAPIView):
+    """A concrete view for creating new users and listing them."""
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+class CreateHangout(generics.ListCreateAPIView):
+    """A concrete view for creating new hangouts and listing them."""
+    queryset = Hangout.objects.all()
+    serializer_class = HangoutSerializer
+
+
+class CreateBrownbag(generics.ListCreateAPIView):
+    """A concrete view for creating new BrownBags and listing them."""
+    queryset = BrownBag.objects.all()
+    serializer_class = BrownbagSerializer
+
+
+class CreateSecretSanta(generics.ListCreateAPIView):
+    """A concrete view for creating new SecretSantas and listing them."""
+    queryset = SecretSanta.objects.all()
+    serializer_class = SecretSantaSerializer
