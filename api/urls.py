cf1f90fd4f78a6cb8a795d28c2bf6c39ccb20ee9
Jee Githinji Gikera <githinjigikera@gmail.com>
2017-02-11 13:44:32 +0300

    [Feature #138140325] add urls that map to the views

---
+++ api/urls.py
@@ -0,0 +1,20 @@
+from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
+from .views import UserView, ProfileView, HangoutView, BrownbagView, \
+    SecretSantaView, HangoutDetailsView, BrownbagDetailsView
+
+
+urlpatterns = {
+    url(r'^users/$', UserView.as_view(), name="users"),
+    url(r'users/(?P<pk>[0-9]+)/$',
+        ProfileView.as_view(), name="profile"),
+    url(r'^hangout/$', HangoutView.as_view(), name="hangout"),
+    url(r'^hangout/(?P<pk>[0-9]+)/$',
+        HangoutDetailsView.as_view(), name="hangout_details"),
+    url(r'^brownbag/$', BrownbagView.as_view(), name="brownbag"),
+    url(r'^brownbag/(?P<pk>[0-9]+)/$',
+        BrownbagDetailsView.as_view(), name="brownbag_details"),
+    url(r'^santa/$', SecretSantaView.as_view(), name="santa")
+}
+
+urlpatterns = format_suffix_patterns(urlpatterns)
