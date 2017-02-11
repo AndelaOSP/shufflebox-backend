aafe99cfbcdf27510c05ed54778edd778103af43
Jee Githinji Gikera <githinjigikera@gmail.com>
2017-02-11 23:12:25 +0300

    (chore) test shuffle view can create groups

--- api/tests/test_views.py
+++ api/tests/test_views.py
@@ -21,7 +21,6 @@ class UserViewTestCase(TestCase):
         self.response = self.client.post(
             '/api/users/', self.user_data, format="json")
 
-
     def test_api_can_create_user(self):
         """Tests that the API has user creation capability."""
         self.assertEquals(self.response.status_code, status.HTTP_201_CREATED)
@@ -32,12 +31,43 @@ class UserViewTestCase(TestCase):
         self.assertEquals(res.status_code, status.HTTP_200_OK)
 
 
+class ShuffleViewTestCase(TestCase):
+    """Test suite for the shuffling view."""
+    def setUp(self):
+        self.client = APIClient()
+        self.hangout = {
+            'type': "hangout", "limit": 6, "notify": True
+        }
+        self.brownbag = {
+            "type": "brownbag", "limit": 1, "notify": True
+        }
+        self.secretsanta = {
+            "type": "secretsanta", "limit": 2, "notify": True
+        }
+
+    def test_view_can_generate_hangout_groups(self):
+        res = self.client.post('/api/shuffle/', self.hangout, format="json")
+        self.assertEquals(res.status_code, status.HTTP_201_CREATED)
+
+    def test_view_can_generate_next_brownbag(self):
+        res = self.client.post('/api/shuffle/', self.brownbag, format="json")
+        self.assertEquals(res.status_code, status.HTTP_201_CREATED)
+
+    def test_view_can_generate_secretsanta_pairs(self):
+        res = self.client.post('/api/shuffle/', self.secretsanta, format="json")
+        self.assertEquals(res.status_code, status.HTTP_201_CREATED)
+
+
 class BrownbagViewTestCase(TestCase):
     """Test suite for the brown bag related views."""
 
     def setUp(self):
         """Set up the test variables."""
-        pass
+        self.client = APIClient()
+        self.brownbag = {
+
+            status: 'nextInLine',
+        }
 
     def test_view_can_get_next_presenter(self):
         """Tests that the API can get the next brown bag presenter."""
