5cd1973b2a23a954d5ff9aedad9438868e844bbf
Jee Githinji Gikera <githinjigikera@gmail.com>
2017-02-11 13:45:04 +0300

    [Feature #138140325] define views that will map to the urls

--- api/views.py
+++ api/views.py
@@ -6,24 +6,42 @@ from django.contrib.auth.models import User
 
 
 class UserView(generics.ListCreateAPIView):
-    """A concrete view for creating new users and listing them."""
+    """A view for creating new users and listing them."""
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
 
-class CreateHangout(generics.ListCreateAPIView):
-    """A concrete view for creating new hangouts and listing them."""
+class ProfileView(generics.RetrieveUpdateDestroyAPIView):
+    """A view for retrieving, updating and deleting a user instance."""
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class HangoutView(generics.ListCreateAPIView):
+    """A view for creating new hangouts and listing them."""
     queryset = Hangout.objects.all()
     serializer_class = HangoutSerializer
 
 
-class CreateBrownbag(generics.ListCreateAPIView):
-    """A concrete view for creating new BrownBags and listing them."""
+class HangoutDetailsView(generics.RetrieveUpdateDestroyAPIView):
+    """A view for retrieving, updating and deleting a hangout instance."""
+    queryset = Hangout.objects.all()
+    serializer_class = HangoutSerializer
+
+
+class BrownbagView(generics.ListCreateAPIView):
+    """A view for creating new BrownBags and listing them."""
     queryset = BrownBag.objects.all()
     serializer_class = BrownbagSerializer
 
 
-class CreateSecretSanta(generics.ListCreateAPIView):
-    """A concrete view for creating new SecretSantas and listing them."""
+class BrownbagDetailsView(generics.RetrieveUpdateDestroyAPIView):
+    """A view for retrieving, updating and deleting a brownbag instance."""
+    queryset = BrownBag.objects.all()
+    serializer_class = BrownbagSerializer
+
+
+class SecretSantaView(generics.ListCreateAPIView):
+    """A view for creating a new SecretSanta pair and listing all pairs."""
     queryset = SecretSanta.objects.all()
     serializer_class = SecretSantaSerializer
