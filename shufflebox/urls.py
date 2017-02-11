1df12668fd72f8ce5116f4bb2f84190509f3fa5c
Jee Githinji Gikera <githinjigikera@gmail.com>
2017-02-11 17:38:22 +0300

    [Feature #138140325] define views and integrate namespace for api urls

--- shufflebox/urls.py
+++ shufflebox/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url('', include('social_django.urls', namespace='social'))
+    url('', include('social_django.urls', namespace='social')),
+    url(r'^api/', include('api.urls', namespace='api'))
 ]
