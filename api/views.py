dc5c59fad3d1c5f488be62c21fd52413d3f93e4a
Jee Githinji Gikera <githinjigikera@gmail.com>
2017-02-11 22:09:40 +0300

    [Feature #138140325] add shuffle view to handle shuffling requests

--- api/views.py
+++ api/views.py
@@ -1,8 +1,12 @@
 from .models import BrownBag, Hangout, SecretSanta, Profile
 from .serializers import UserSerializer, ProfileSerializer, \
     BrownbagSerializer, HangoutSerializer, SecretSantaSerializer
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.exceptions import ParseError
 from django.contrib.auth.models import User
+from datetime import datetime
 
 
 class UserView(generics.ListCreateAPIView):
@@ -15,6 +19,68 @@ class ProfileView(generics.RetrieveUpdateDestroyAPIView):
     """A view for retrieving, updating and deleting a user instance."""
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+class ShuffleView(APIView):
+    """A view for handling shuffling requests from consumption clients."""
+
+    def post(self, request):
+        """
+        Return a query set according to the post message status.
+        """
+        try:
+            request_type = request.data.pop('type')
+            if request_type == "brownbag":
+                # Create the next brownbag
+                # dummy data simulated from the shufflebox module
+                data = {
+                    "date": str(datetime.now().date()),
+                    "status": "nextInLine",
+                    "user_id": 1
+                }
+                serializer = BrownbagSerializer(data=data)
+                if serializer.is_valid():
+                    serializer.save()
+                    return Response(
+                        serializer.data, status=status.HTTP_201_CREATED)
+                return Response(
+                    serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+            elif request_type == "hangout":
+                # Create hangout groups for the month
+                # dummy data simulated from the shufflebox module
+                data = {
+                    "date": str(datetime.now().date()),
+                    "status": "nextInLine",
+                    "user_id": 1
+                }
+                serializer = HangoutSerializer(data=data)
+                if serializer.is_valid():
+                    serializer.save()
+                    return Response(
+                        serializer.data, status=status.HTTP_201_CREATED)
+                return Response(status=status.HTTP_400_BAD_REQUEST)
+
+            elif request_type == "secretsanta":
+                # Create all secretsanta pairs for that year
+                # dummy data simulated from the shufflebox module
+                data = {
+                    "date": str(datetime.now().date()),
+                    "santa": 3,
+                    "giftee": 1
+                }
+                serializer = SecretSantaSerializer(data=data)
+                if serializer.is_valid():
+                    serializer.save()
+                    return Response(
+                        serializer.data, status=status.HTTP_201_CREATED)
+                return Response(status=status.HTTP_400_BAD_REQUEST)
+            else:
+                return Response(
+                    "Error: Bad Request", status=status.HTTP_400_BAD_REQUEST)
+        except ParseError:
+            return Response(
+                "Error: bad request", status=status.HTTP_400_BAD_REQUEST)
 
 
 class HangoutView(generics.ListCreateAPIView):
