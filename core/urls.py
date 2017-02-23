43bb3573835e091fe93d5d47dde5bbce635907ba
Alex Kiura <kiuraalex@gmail.com>
2017-02-23 17:39:13 +0300

    [Feature #138315199] add social auth url

--- core/urls.py
+++ core/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url('', include('social_django.urls', namespace='social')),
-    url(r'^api/', include('api.urls', namespace='api'))
+    url(r'^api/', include('api.urls', namespace='api')),
+    url(r'^login', TemplateView.as_view(template_name='google_login.html')),
+
 ]
