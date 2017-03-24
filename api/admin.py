0c6fabcd528e8d72623a13985470534da69075d9
Jee Githinji Gikera <githinjigikera@gmail.com>
2017-03-24 10:47:32 +0300

    [Fix] refactor brownbag model with group and profile serializers

--- api/admin.py
+++ api/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Profile, BrownBag, SecretSanta, Hangout
+from .models import Profile, Brownbag, SecretSanta, Hangout
 
 # Register your models here.
 admin.site.register(Profile)
-admin.site.register(BrownBag)
+admin.site.register(Brownbag)
 admin.site.register(Hangout)
 admin.site.register(SecretSanta)
