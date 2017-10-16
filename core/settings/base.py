a3a71c014467e1512ef897ad69b37d1826e6f253
Boniface Mwenda <boniface.mwenda@andela.com>
2017-10-16 11:11:45 +0300

    [Fix] whitelist frontend url

--- core/settings/base.py
+++ core/settings/base.py
@@ -76,7 +76,7 @@ MIDDLEWARE = [
 CORS_ORIGIN_WHITELIST = (
     'localhost:3000',
     '127.0.0.1:3000',
-    'https://shufflebox-frontend.herokuapp.com'
+    'shufflebox-frontend.herokuapp.com'
 )
 
 CORS_ALLOW_CREDENTIALS = True
