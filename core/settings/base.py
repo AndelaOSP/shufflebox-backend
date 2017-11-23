a863b0865e5ecafc53538856fbaa39a2bbc224f7
DNG <denis.gathondu@andela.com>
2017-11-23 16:23:41 +0300

    merge develop

--- core/settings/base.py
+++ core/settings/base.py
@@ -173,7 +173,7 @@ ADMINS = [
 
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
 EMAIL_HOST = config('EMAIL_HOST', default='')
-EMAIL_PORT = config('EMAIL_PORT' , default=None, cast=int)
+EMAIL_PORT = config('EMAIL_PORT' , default='', cast=int)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
 DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='')
