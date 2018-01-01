b5b719f7e6d1b88c5a0f9163b9e2d3380021bbd3
DNG <denis.gathondu@andela.com>
2018-01-01 23:15:37 +0300

    Pep8 fixes
    Signed-off-by: DNG <denis.gathondu@andela.com>

--- api/utils.py
+++ api/utils.py
@@ -12,26 +12,29 @@ class DefaultMail(object):
     """
     Common email class settings
     """
+
     def __init__(self):
         self.message_list = []
         self.subject = 'No subject'
         self.message = 'No message'
-        self.from_email = "{} <{}>".format(settings.ADMIN_NAME.upper() ,settings.DEFAULT_FROM_EMAIL)
+        self.from_email = "{} <{}>".format(settings.ADMIN_NAME.upper(), settings.DEFAULT_FROM_EMAIL)
         self.recipients = []
-
 
 
 class MailGun(DefaultMail):
     """
     Class to handle mail gun emails
     """
+
     def single_mail(self):
         send_mail(self.subject, self.message, self.from_email, self.recipients)
+
 
 class SendMail(DefaultMail):
     """
     Class to handle sending of emails
     """
+
     def __init__(self):
         self.send_grid = sendgrid.SendGridAPIClient(api_key=settings.SENDGRID_API_KEY)
         self.from_email = Email(email=settings.DEFAULT_FROM_EMAIL, name='SHUFFLEBOX')
@@ -45,7 +48,8 @@ class SendMail(DefaultMail):
                     admin_name = admin[0]
                     recipient = Email(admin[1])
                     content = Content('text/html', self.message)
-                    mail_msg = Mail(from_email=self.from_email, subject=self.subject, to_email=recipient, content=content)
+                    mail_msg = Mail(from_email=self.from_email, subject=self.subject, to_email=recipient,
+                                    content=content)
                     mail_msg.personalizations[0].add_substitution(Substitution(key='-admin-', value=admin_name))
                     mail_msg.personalizations[0].add_substitution(Substitution(key='-message-', value=self.message))
                     mail_msg.template_id = settings.ADMIN_TEMPLATE
@@ -55,7 +59,6 @@ class SendMail(DefaultMail):
                     "Admin list should not be empty")
         except urllib.HTTPError as e:
             return str(e)
-
 
     def santa_message(self, message=None, giftee_email=None, recipient=None, santa_name=None):
         try:
--- core/settings/base.py
+++ core/settings/base.py
@@ -17,7 +17,6 @@ from decouple import config
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
@@ -105,7 +104,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'core.wsgi.application'
 
-
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
 
@@ -161,7 +159,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
@@ -190,4 +187,3 @@ SLACK_TOKEN = config('SLACK_TOKEN', default='')
 SENDGRID_API_KEY = config('SENDGRID_API_KEY', default='')
 ADMIN_TEMPLATE = config('ADMIN_TEMPLATE_ID', default='')
 SANTA_TEMPLATE = config('SANTA_TEMPLATE_ID', default='')
-
